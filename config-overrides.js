23f91d36d7748a1ceb64a37b66ad3c7a7f8e8b1b
jcy <542405873@qq.com>
2019-10-23 13:35:03 +0800

    删去了不必要的文件，修改了webpack配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,30 @@
 // config-overrides
-const { override, addBabelPlugins } = require("customize-cra");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+const {
+  override,
+  addBabelPlugins,
+  addWebpackPlugin,
+  addDecoratorsLegacy
+} = require("customize-cra");
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+const babelPlugins = ["lodash"];
+const webpackPlugins = [new LodashModuleReplacementPlugin()];
+const PRODCondition = ["production", "prod", "staging", "stage", "uat", "qa"];
+const IS_PROD = PRODCondition.includes(process.env.NODE_ENV);
+
+if (IS_PROD) {
+  babelPlugins.push("transform-remove-console");
+  babelPlugins.push("transform-remove-debugger");
+  webpackPlugins.push(new BundleAnalyzerPlugin());
+}
 
 module.exports = override(
-  ...addBabelPlugins([
-    "@babel/plugin-proposal-decorators",
-    {
-      legacy: true
-    }
-  ]),
-  config => {
-    if (config.mode === "production") {
-      config.plugins.push(new BundleAnalyzerPlugin());
-    }
-    return config;
-  }
+  addDecoratorsLegacy(),
+  ...addBabelPlugins(babelPlugins),
+  ...webpackPlugins.map(plugin => addWebpackPlugin(plugin))
+  // config => {
+  //   config.plugins.push(...webpackPlugins);
+  //   return config;
+  // }
 );
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import styles from "./App.module.scss";
+// import _ from "lodash";
+
+// const a = {
+//   b: {
+//     c: "1"
+//   }
+// };
+// console.log(_.get(a, "b.c", ""), "");
+// console.log(...a);
 
 const App: React.FC = () => {
   return (
