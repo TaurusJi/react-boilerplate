ab4daf70b0fb57aae18d80d839ef71a1123c0f6f
jcy <542405873@qq.com>
2019-11-08 16:33:44 +0800

    BasicLayout改hook

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, useMemo } from "react";
 import { Layout, Breadcrumb, Icon, Dropdown, Avatar, Menu } from "antd";
 import { RouteComponentProps } from "react-router-dom";
 import MenuSider from "../components/Sider";
@@ -21,6 +21,9 @@ import {
   FooterCss,
   LayoutCss
 } from "./style";
+import { useSelector } from "react-redux";
+import { State } from "src/store/reducers";
+import useReactRouter from "use-react-router";
 
 const { Sider } = Layout;
 
@@ -28,134 +31,245 @@ interface IProps {
   route: Partial<RouteModel>;
 }
 
-@connect<{}, RouteComponentProps & IProps>(state => {
-  const pathname = state.router.location.pathname;
-  const match = matchRoutes(routes, pathname);
+const useRoute = () => {
+  const { location } = useSelector((state: State) => state.router);
+  const route: RouteModel = useMemo(() => {
+    const match = matchRoutes(routes, location.pathname) as RouteModel[];
+    return get(match[match.length - 1], "route", {});
+  }, [location.pathname]);
 
-  return { route: get(match[match.length - 1], "route", {}) };
-})
-class AuthorizedLayout extends PureComponent<RouteComponentProps & IProps> {
-  static defaultProps = {
-    route: {}
-  };
+  return { route };
+};
 
-  static getDerivedStateFromProps() {
-    nProgress.start();
+const renderBreadcrumb = () => {
+  const { route } = useRoute();
+  const breadcrumbData = useMemo(() => {
+    const { breadcrumb } = route;
+    return breadcrumb ? generateBreadcrumb(breadcrumb) : [];
+  }, [route]);
+
+  return (
+    <Breadcrumb style={{ marginBottom: "16px" }}>
+      {breadcrumbData.map(item => (
+        <Breadcrumb.Item key={item.href}>{item.text}</Breadcrumb.Item>
+      ))}
+    </Breadcrumb>
+  );
+};
+
+const renderHeader = () => {
+  const userMenu = (
+    <MenuCss>
+      <Menu.Item disabled className="user-menu-item">
+        <Icon type="user" className="user-menu-icon" />
+        <span>个人中心</span>
+      </Menu.Item>
+      <Menu.Item disabled className="user-menu-item">
+        <Icon type="setting" className="user-menu-icon" />
+        <span>设置</span>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item className="user-menu-item">
+        <div role="presentation">
+          <Icon type="logout" className="user-menu-icon" />
+          <span>退出登陆</span>
+        </div>
+      </Menu.Item>
+    </MenuCss>
+  );
+
+  return (
+    <HeaderCss>
+      <Dropdown overlay={userMenu} placement="bottomRight">
+        <div className="avatar-container">
+          <Avatar className="avatar">N</Avatar>
+        </div>
+      </Dropdown>
+    </HeaderCss>
+  );
+};
+
+const renderPageHeader = () => {
+  const { route } = useRoute();
+
+  if (isEmpty(route.name)) {
     return null;
   }
 
-  componentDidMount() {
-    nProgress.done();
-  }
-
-  componentDidUpdate() {
-    nProgress.done();
-  }
-
-  renderBreadcrumb = () => {
-    const {
-      route: { breadcrumb }
-    } = this.props;
-
-    if (!breadcrumb) {
-      return null;
-    }
-
-    const breadcrumbData = generateBreadcrumb(breadcrumb);
-
-    return (
-      <Breadcrumb style={{ marginBottom: "16px" }}>
-        {breadcrumbData.map(item => (
-          <Breadcrumb.Item key={item.href}>{item.text}</Breadcrumb.Item>
-        ))}
-      </Breadcrumb>
-    );
-  };
-
-  renderHeader = () => {
-    const userMenu = (
-      <MenuCss>
-        <Menu.Item disabled className="user-menu-item">
-          <Icon type="user" className="user-menu-icon" />
-          <span>个人中心</span>
-        </Menu.Item>
-        <Menu.Item disabled className="user-menu-item">
-          <Icon type="setting" className="user-menu-icon" />
-          <span>设置</span>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item className="user-menu-item">
-          <div role="presentation">
-            <Icon type="logout" className="user-menu-icon" />
-            <span>退出登陆</span>
-          </div>
-        </Menu.Item>
-      </MenuCss>
-    );
-
-    return (
-      <HeaderCss>
-        <Dropdown overlay={userMenu} placement="bottomRight">
-          <div className="avatar-container">
-            <Avatar className="avatar">N</Avatar>
-          </div>
-        </Dropdown>
-      </HeaderCss>
-    );
-  };
-
-  renderPageHeader = () => {
-    const {
-      route: { name }
-    } = this.props;
-
-    if (isEmpty(name)) {
-      return null;
-    }
-
-    return (
-      <PageHeaderCss>
-        {this.renderBreadcrumb()}
-        <div className="page-title">{name}</div>
-      </PageHeaderCss>
-    );
-  };
-
-  renderContent = () => {
-    const { children } = this.props;
-    return <ContentCss>{children}</ContentCss>;
-  };
-
-  renderFooter = () => (
-    <FooterCss className="footer">Copyright © 2019</FooterCss>
+  return (
+    <PageHeaderCss>
+      {renderBreadcrumb()}
+      <div className="page-title">{route.name}</div>
+    </PageHeaderCss>
   );
+};
 
-  render() {
-    const {
-      location: { pathname }
-    } = this.props;
+const renderContent = (children: React.ReactNode) => {
+  return <ContentCss>{children}</ContentCss>;
+};
 
-    return (
-      <LoginChecker>
-        <Layout>
-          <Sider width={256} style={{ minHeight: "100vh" }}>
-            <MenuSider
-              menuData={menuData}
-              pathname={pathname}
-              appLogo={logo}
-              appName="React Boilerplate"
-            ></MenuSider>
-          </Sider>
-          <LayoutCss>
-            {this.renderHeader()}
-            {this.renderPageHeader()}
-            {this.renderContent()}
-            {this.renderFooter()}
-          </LayoutCss>
-        </Layout>
-      </LoginChecker>
-    );
-  }
-}
+const renderFooter = () => (
+  <FooterCss className="footer">Copyright © 2019</FooterCss>
+);
 
-export default AuthorizedLayout;
+const BasicLayout: React.FC = props => {
+  const { history } = useReactRouter();
+  const {
+    location: { pathname }
+  } = history;
+
+  return (
+    <LoginChecker>
+      <Layout>
+        <Sider width={256} style={{ minHeight: "100vh" }}>
+          <MenuSider
+            menuData={menuData}
+            pathname={pathname}
+            appLogo={logo}
+            appName="React Boilerplate"
+          ></MenuSider>
+        </Sider>
+        <LayoutCss>
+          {renderHeader()}
+          {renderPageHeader()}
+          {renderContent(props.children)}
+          {renderFooter()}
+        </LayoutCss>
+      </Layout>
+    </LoginChecker>
+  );
+};
+
+export default BasicLayout;
+
+// @connect<{}, RouteComponentProps & IProps>(state => {
+//   const pathname = state.router.location.pathname;
+//   const match = matchRoutes(routes, pathname);
+
+//   return { route: get(match[match.length - 1], "route", {}) };
+// })
+// class AuthorizedLayout extends PureComponent<RouteComponentProps & IProps> {
+//   static defaultProps = {
+//     route: {}
+//   };
+
+//   static getDerivedStateFromProps() {
+//     nProgress.start();
+//     return null;
+//   }
+
+//   componentDidMount() {
+//     nProgress.done();
+//   }
+
+//   componentDidUpdate() {
+//     nProgress.done();
+//   }
+
+//   renderBreadcrumb = () => {
+//     const {
+//       route: { breadcrumb }
+//     } = this.props;
+
+//     if (!breadcrumb) {
+//       return null;
+//     }
+
+//     const breadcrumbData = generateBreadcrumb(breadcrumb);
+
+//     return (
+//       <Breadcrumb style={{ marginBottom: "16px" }}>
+//         {breadcrumbData.map(item => (
+//           <Breadcrumb.Item key={item.href}>{item.text}</Breadcrumb.Item>
+//         ))}
+//       </Breadcrumb>
+//     );
+//   };
+
+//   renderHeader = () => {
+//     const userMenu = (
+//       <MenuCss>
+//         <Menu.Item disabled className="user-menu-item">
+//           <Icon type="user" className="user-menu-icon" />
+//           <span>个人中心</span>
+//         </Menu.Item>
+//         <Menu.Item disabled className="user-menu-item">
+//           <Icon type="setting" className="user-menu-icon" />
+//           <span>设置</span>
+//         </Menu.Item>
+//         <Menu.Divider />
+//         <Menu.Item className="user-menu-item">
+//           <div role="presentation">
+//             <Icon type="logout" className="user-menu-icon" />
+//             <span>退出登陆</span>
+//           </div>
+//         </Menu.Item>
+//       </MenuCss>
+//     );
+
+//     return (
+//       <HeaderCss>
+//         <Dropdown overlay={userMenu} placement="bottomRight">
+//           <div className="avatar-container">
+//             <Avatar className="avatar">N</Avatar>
+//           </div>
+//         </Dropdown>
+//       </HeaderCss>
+//     );
+//   };
+
+//   renderPageHeader = () => {
+//     const {
+//       route: { name }
+//     } = this.props;
+
+//     if (isEmpty(name)) {
+//       return null;
+//     }
+
+//     return (
+//       <PageHeaderCss>
+//         {this.renderBreadcrumb()}
+//         <div className="page-title">{name}</div>
+//       </PageHeaderCss>
+//     );
+//   };
+
+//   renderContent = () => {
+//     const { children } = this.props;
+//     return <ContentCss>{children}</ContentCss>;
+//   };
+
+//   renderFooter = () => (
+//     <FooterCss className="footer">Copyright © 2019</FooterCss>
+//   );
+
+//   render() {
+//     const {
+//       location: { pathname }
+//     } = this.props;
+
+//     return (
+//       <LoginChecker>
+//         <Layout>
+//           <Sider width={256} style={{ minHeight: "100vh" }}>
+//             <MenuSider
+//               menuData={menuData}
+//               pathname={pathname}
+//               appLogo={logo}
+//               appName="React Boilerplate"
+//             ></MenuSider>
+//           </Sider>
+//           <LayoutCss>
+//             {this.renderHeader()}
+//             {this.renderPageHeader()}
+//             {this.renderContent()}
+//             {this.renderFooter()}
+//           </LayoutCss>
+//         </Layout>
+//       </LoginChecker>
+//     );
+//   }
+// }
+
+// export default AuthorizedLayout;
---
+++ src/styles/Mixins.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ellipsis = ({
+  component = null,
+  clamp = 1
+}: {
+  component: any;
+  clamp: number;
+}) => {
+  if (clamp > 1) {
+    return styled(component)`
+      overflow: hidden;
+      display: -webkit-box;
+      text-overflow: ellipsis;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: ${clamp};
+    `;
+  } else {
+    return styled(component)`
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    `;
+  }
+};
