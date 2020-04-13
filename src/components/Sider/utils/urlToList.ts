df73b306d22ca676114c82c75108c338cee3f23c
Ji Chenyan <jicy@anchnet.com>
2020-04-13 14:34:30 +0800

    mod

--- src/components/Sider/utils/urlToList.ts
+++ src/components/Sider/utils/urlToList.ts
@@ -1,6 +1,6 @@
 const urlToList = (url: string) => {
   if (url) {
-    const urlList = url.split("/").filter(i => i);
+    const urlList = url.split("/").filter((i) => i);
     return urlList.map(
       (_, index) => `/${urlList.slice(0, index + 1).join("/")}`
     );
